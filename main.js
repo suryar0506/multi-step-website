04626cb653133a8c004b9eac5d6295b3ff2037bb
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2020-10-19 21:22:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function display() {
+    document.getElementById(drawing.src = "Screen Shot 2020-10-14 at 5.10.36 PM.png");
+    window.alert("This drawing was made by Vincent Van Gogh.Its title is, Starry Night.");
+}
+
+function display1() {
+    document.getElementById(drawing.src = "Screen Shot 2020-10-14 at 5.14.58 PM.png");
+    window.alert("This painting is, The Scream,by Edward Munch");
+}
+
+function change() {
+    document.getElementById(drawing.src = "Screen Shot 2020-10-14 at 6.41.47 PM.png");
+    document.getElementById("draw").style.display = "none";
+    document.getElementById("heading").innerHTML = "My Favorite Character";
+    document.getElementById("head").innerHTML = "Aangie!"
+    document.getElementById("instruct").style.display = "block";
+    document.getElementById("hed").style.display = "none";
+    document.getElementById("para").innerHTML = "My favorite character would have to be Aang,the avatar from The Last Airbender.He is witty,funny,powerful,humble,and dependable. He makes everyone as happy as he can,and if the person's not happy, he takes the time to find out why, and solves the problem, if he can. For example, even though a certain village hated him because he was the Avatar, he went through their unfair court system,and was ready to face their punishment of death! Due to all that I've stated,he is my absolute favorite character.";      
+}
+function changes() {
+    document.getElementById(drawing.src = "Screen Shot 2020-10-14 at 5.10.36 PM.png");
+    document.getElementById("draw").style.display = "block";
+    document.getElementById("instruct").style.display = "none";
+    document.getElementById("heading").innerHTML = "My Hobbie";
+    document.getElementById("head").innerHTML = "Drawing"
+    document.getElementById("hed").innerHTML = "Drawing"
+    document.getElementById("para").innerHTML = "My favorite hobby is drawing. Whenever I have free time, I draw a character or scene from somewhere. I especially like doing scenes from the Hindu Culture.After sketching the drawing with a pencil,I then, proceed to outline it with a black marker,or pen. Finally, I color the drawing with a oil pastel, or water color. Recently,I've also used color pencil. In the near future, I will use acrylic paint. When drawing, one <b>must</b> pay close attention to detail.If one draws sincerely, then automatically,the person will focus pinpointedly.Without focus,patience,and detailed observation,drawing will be a mystery,none can solve.But when I draw sincerely, all thought but the ones about the drawing itself, are removed.I am 12 years old, and study in 7th grade.";      
+}
+function invis() {
+    document.getElementById("instruct").style.display = "none";
+}
